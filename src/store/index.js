1fd694c2ef4fb1e1adcf0fe664919a9ab7c58097
tumelo-sebego <145678194+tumelo-sebego@users.noreply.github.com>
2025-06-12 10:51:22 +0000

    modified store index file

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,19 @@
-wedeliver99-website
-├── src
-│   ├── assets
-│   │   └── styles
-│   │       └── main.css
-│   ├── components
-│   │   └── HelloWorld.vue
-│   ├── router
-│   │   └── index.js
-│   ├── store
-│   │   └── index.js
-│   ├── views
-│   │   ├── HomeView.vue
-│   │   └── AboutView.vue
-│   ├── App.vue
-│   └── main.js
-├── public
-│   └── index.html
-├── tests
-│   └── unit
-│       └── example.spec.js
-├── .gitignore
-├── babel.config.js
-├── vue.config.js
-├── package.json
-└── README.md
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    // your state here
+  },
+  mutations: {
+    // your mutations here
+  },
+  actions: {
+    // your actions here
+  },
+  modules: {
+    // your modules here
+  }
+})
